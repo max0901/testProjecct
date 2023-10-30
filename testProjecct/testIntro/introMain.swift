39ee2d2fbc86c0aa8ebe41114196e7f23ee68f28
leejeongmin <maxsdf@naver.com>
2023-10-30 17:10:17 +0900

    'swift연습'

---
+++ testProjecct/testIntro/introMain.swift
@@ -0,0 +1,18 @@
+//
+//  introMain.swift
+//  testProjecct
+//
+//  Created by 이정민 on 10/30/23.
+//
+
+import SwiftUI
+
+struct introMain: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    introMain()
+}
---
+++ testProjecct/testIntro/textBox.swift
@@ -0,0 +1,18 @@
+//
+//  textBox.swift
+//  testProjecct
+//
+//  Created by 이정민 on 10/30/23.
+//
+
+import SwiftUI
+
+struct textBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    textBox()
+}
